9c01eab6ec1cde634d54b3c1411fe42c22bfd0ba
Syed Shams Islam <isyed4916@gmail.com>
2025-07-22 15:41:34 +0600

    some important xdg specs export

--- bash/.bashrc.d/exports.sh
+++ bash/.bashrc.d/exports.sh
@@ -1,3 +1,7 @@
 export EDITOR="nvim"
 export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_CACHE_HOME="$HOME/.cache"
 # export PATH="$HOME/bin:$PATH" #Syntax for appending to PATH
