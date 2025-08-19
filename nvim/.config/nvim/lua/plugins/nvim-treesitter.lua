f938d3f105d87046e914a8e1be765edda88ae04a
Syed Shams Islam <isyed4916@gmail.com>
2025-08-20 04:28:24 +0600

    nvim colorscheme and treesitter

---
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,76 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    main = "nvim-treesitter.configs",
+    opts = {
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "latex", "bash" },
+        sync_install = false,
+        auto_install = false,
+        highlight = {
+            enable = true,
+            disable = function(lang, buf)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
+            additional_vim_regex_highlighting = false
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-i>",
+                node_incremental = "<C-i>",
+                scope_incremental = false,
+                node_decremental = "<bs>"
+            }
+        },
+        indent = { enable = true },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                },
+            },
+            move = {
+                enable = true,
+                set_jumps = true,
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                },
+            },
+            swap = {
+                enable = true,
+                swap_next = {
+                    ["<leader>a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["<leader>A"] = "@parameter.inner",
+                },
+            },
+        },
+    }
+}
